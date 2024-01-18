eb6c2a4d2f7c129872f1ddb43e79a92901284b98
Tony Diep <tonydiep@tonydiep.com>
2024-01-17 21:23:36 -0500

    Refactored into functions

--- ThrottleIfOver.sh
+++ ThrottleIfOver.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-## start unthrottled
-unthrottle
-
 # Don't let CPU go over this temperature
-max_temp=$1
-
-if [[ $max_temp -eq null ]]; then
+if [ "$#" -gt 0 ]; then
+    max_temp=$1
+else
     # use safe default
     max_temp=65
 fi
@@ -24,43 +21,45 @@ threshold=$((max_temp - padding))
 
 throttled=false
 
-while true; do
-    current_temp=$(cat /sys/class/thermal/thermal_zone0/temp)
-    current_temp=$((current_temp / 1000))
 
-    if [[ $current_temp -gt $max_temp ]]; then
-        # echo $(date) "$current_temp > $max_temp: Throttling"
-        throttle
-    elif [[ $throttled ]] && [[ $current_temp -lt $threshold ]]; then
-        # echo $(date) "$current_temp < $threshold: Unthrottling"
-        unthrottle
-    fi
-    # echo $(date) "$current_temp"
-    sleep 1
-done
-
-unthrottle() {
+function unthrottle() {
     sudo cpufreq-set -c 0 --max 1516MHz
     sudo cpufreq-set -c 1 --max 1516MHz
     sudo cpufreq-set -c 2 --max 1516MHz
     sudo cpufreq-set -c 3 --max 1516MHz
     sudo cpufreq-set -c 4 --max 2016MHz
     sudo cpufreq-set -c 5 --max 2016MHz
-
-    throttled=false
 }
 
-throttle() {
+function throttle() {
     sudo cpufreq-set -c 0 --max 408MHz
     sudo cpufreq-set -c 1 --max 408MHz
     sudo cpufreq-set -c 2 --max 408MHz
     sudo cpufreq-set -c 3 --max 408MHz
     sudo cpufreq-set -c 4 --max 408MHz
     sudo cpufreq-set -c 5 --max 408MHz
-
-    throttled=true
 }
 
-throttleB(){
-    
-}
+## start unthrottled
+unthrottle
+
+# Main Loop
+#
+while true; do
+    current_temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+    current_temp=$((current_temp / 1000))
+
+    if [[ $current_temp -gt $max_temp ]]; then
+        echo $(date) "$current_temp > $max_temp: Throttling"
+        throttle
+        throttled=true
+    elif $throttled ; then
+        if [[ $current_temp -lt $threshold ]]; then
+            echo $(date) "$current_temp < $threshold: Unthrottling"
+            unthrottle
+            unthrottled=true
+        fi
+    fi
+    # echo $(date) "$current_temp"
+    sleep 1
+done
